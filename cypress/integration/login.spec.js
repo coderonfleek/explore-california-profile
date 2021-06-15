5e18d65b615c153094ab931192f3c13d9d1bb8f3
Fikayo <fikfikky@gmail.com>
2021-06-15 13:54:34 +0100

    Cypress testing - Completed

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,6 +2,14 @@ describe("Login test", () => {
 
     it("Can login with email and password", () => {
 
+        //Navigate to home page
         cy.visit("http://localhost:8080");
+
+        cy.get(".login-email").type("test@mail.com");
+        cy.get(".login-password").type("test");
+
+        cy.get(".login-button").click();
+
+        cy.url().should("include", "/profile");
     })
 })
